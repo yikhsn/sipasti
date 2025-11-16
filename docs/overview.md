36f4cad6ae175f23c7f5ba69472a8e17f835bc3b
Yaumil Ikhsan <yikhsn@gmail.com>
2025-11-16 14:36:51 +0700

    Project init setup done

---
+++ docs/overview.md
@@ -0,0 +1,18 @@
+---
+sidebar_position: 1
+---
+
+# Overview Sistem
+
+**Rawat ID** adalah sistem informasi pengelolaan fasilitas pelayanan kesehatan modern untuk meningkatkan pelayanan pasien di fisilitas kesehatan, memudahkan petugas untuk mengelola data pasien dan memudahkan petugas kesehatan dalam menyusun rekam medis.
+
+**Rawat ID** memiliki fitur lengkap yang dirancang untuk bisa digunakan dengan mudah untuk berbagai fasilitas pelayan kesehatan (faskes) seperti Rumah Sakit, Klinik, Apotik hingga Praktik Dokter Mandiri. 
+
+
+## Bagaimana Memulai?
+
+Jika faskesmu belum berlangganan **Rawat ID**, pastikan faskesmu untuk berlangganan **Rawat ID** terlebih dahulu untuk mulai menggunakan!
+
+### Apa yang Kamu Butuhkan?
+
+Untuk memulai menggunakan Rawat ID, pastikan faskesmu sudah menggunakan **Rawat ID** dan kamu memiliki akses untuk menggunakannya, baik link (tautan) sistem Rawat ID faskesmu, akun pengguna (username) dan kata sandi (password)
---
+++ docusaurus.config.js
@@ -0,0 +1,158 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Panduan Pengguna - Rawat ID',
+  tagline: 'Sistem Informasi Manajemen Rumah Sakit dan Klinik Andalan Tenaga Kesehatan!',
+  favicon: 'img/rawatid.png',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://guideline.rawat.id',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'rawatid', // Usually your GitHub org/user name.
+  projectName: 'rawat-03-guideline', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'id',
+    locales: ['id'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: 'Guideline Rawat ID',
+        logo: {
+          alt: 'Logo Rawat ID',
+          src: 'img/rawatid.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Panduan Pengguna',
+          },
+          {to: '/blog', label: 'Catatan Rilis', position: 'left'},
+          {
+            href: 'https://demo.rawat.id/',
+            label: 'Demo',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Guideline',
+            items: [
+              {
+                label: 'Panduan Pengguna',
+                to: '/docs/overview',
+              },
+            ],
+          },
+          {
+            title: 'Tautan',
+            items: [
+              {
+                label: 'Demo',
+                href: 'https://demo.rawat.id/',
+              },
+              {
+                label: 'Instagram',
+                href: 'https://instagram.com/rawat_indonesia',
+              },
+              {
+                label: 'Blog Rawat ID',
+                href: 'https://blog.rawat.id/',
+              },
+            ],
+          },
+          {
+            title: 'Lainnya',
+            items: [
+              {
+                label: 'Catatan Rilis',
+                to: '/blog',
+              },
+               {
+                label: 'Catatan Pengembang',
+                href: 'https://dev.rawat.id/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Rawat ID`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
